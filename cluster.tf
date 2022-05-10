20c5d902aa6440644a8506d621525c6c48fc246a
priyankapeelu <priyankapeelu@icloud.com>
2022-05-10 13:59:31 +0530

    terraform variablescd tt

--- cluster.tf
+++ cluster.tf
@@ -1,18 +1,18 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "roboshop-${var.ENV}"
   engine               = "redis"
-  node_type            = "var.ELASTICACHE_NODE_TYPE"
-  num_cache_nodes      = "var.ELASTICACHE_NODE_COUNT"
+  node_type            = var.ELASTICACHE_NODE_TYPE
+  num_cache_nodes      = var.ELASTICACHE_NODE_COUNT
   parameter_group_name = aws_elasticache_parameter_group.default.name
-  engine_version       = "var.ELASTICACHE_ENGINE_VERSION"
-  port                 = "var.ELASTICACHE_PORT"
+  engine_version       = var.ELASTICACHE_ENGINE_VERSION
+  port                 = var.ELASTICACHE_PORT
   subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
   security_group_ids   = [aws_security_group.allow_redis.id]
 }
 
 resource "aws_elasticache_parameter_group" "default" {
   name   = "roboshop-${var.ENV}"
-  family = "redis${"var.ELASTICACHE_ENGINE_VERSION}"
+  family = "redis${var.ELASTICACHE_ENGINE_VERSION}"
 }
 
 resource "aws_elasticache_subnet_group" "subnet-group" {
@@ -28,7 +28,7 @@ resource "aws_security_group" "allow_redis" {
   ingress {
     description = "TLS from VPC"
     from_port   = var.ELASTICACHE_PORT
-    to_port     =var.ELASTICACHE_PORT
+    to_port     = var.ELASTICACHE_PORT
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }

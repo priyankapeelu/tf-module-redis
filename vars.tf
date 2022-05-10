ecd293ab7178ad8cb97508552738ea1a30ca97e1
priyankapeelu <priyankapeelu@icloud.com>
2022-05-10 13:56:30 +0530

    terraform variablescd tt

--- vars.tf
+++ vars.tf
@@ -1,1 +1,7 @@
 variable "ENV" {}
+
+variable "ENV" {}
+variable "ELASTICACHE_NODE_TYPE" {}
+variable "ELASTICACHE_NODE_COUNT" {}
+variable "ELASTICACHE_PORT" {}
+variable "ELASTICACHE_ENGINE_VERSION" {}
